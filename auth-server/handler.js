175fc8f936e6f84edd71b5215748e0fb3dce5210
webcodejunkie <webcodejunkie@gmail.com>
2021-11-14 21:10:06 -0500

    added another serverless function and a static webpage

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -26,6 +26,7 @@ const oAuth2Client = new google.auth.OAuth2(
 module.exports.getAuthURL = async () => {
 
   const authUrl = oAuth2Client.generateAuthUrl({
+
     access_type: "offline",
     scope: SCOPES,
   });
@@ -39,4 +40,42 @@ module.exports.getAuthURL = async () => {
       authUrl: authUrl,
     }),
   };
+};
+
+module.exports.getAcessToken = async (event) => {
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    /**
+     * Exhange authorization code for access token with a "callback" after the exhange,
+     * The callback in this case is an arrow function with the results as paramters: "err" and "token.""
+     */
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(token);
+    });
+  })
+    .then((token) => {
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        body: JSON.stringify(token),
+      };
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
 };
