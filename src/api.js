33cf1b3c578a7bd559911808619415b3fd845d6b
webcodejunkie <webcodejunkie@gmail.com>
2021-12-09 15:07:18 -0500

    fixed api typo

--- src/api.js
+++ src/api.js
@@ -58,7 +58,7 @@ export const getAccessToken = async () => {
       const { authUrl } = results.data;
       return (window.location.href = authUrl);
     }
-    return code && getAcessToken(code);
+    return code && getAccessToken(code);
   }
   return
 }
