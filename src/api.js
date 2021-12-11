f34b0deeeb7c2aa45da0e5562b68f63efe2e0619
webcodejunkie <webcodejunkie@gmail.com>
2021-12-11 16:14:37 -0500

    fix variable typo

--- src/api.js
+++ src/api.js
@@ -57,9 +57,9 @@ export const getAccessToken = async () => {
       const { authUrl } = results.data;
       return (window.location.href = authUrl);
     }
-    return code && getAccessToken(code);
+    return code && getToken(code);
   }
-  return
+  return accessToken;
 }
 
 const removeQuery = () => {
