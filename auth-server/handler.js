9b030d3efd8e0b779573fce6a59a17ac2e29ac58
webcodejunkie <webcodejunkie@gmail.com>
2021-12-10 20:56:26 -0500

    Revert "Updates"
    This reverts commit 69fc056a5e0399324326263b51887932bd6e342a.

---
+++ auth-server/handler.js
@@ -0,0 +1,134 @@
+const { google } = require("googleapis");
+
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar("v3");
+
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+const credentials = {
+  client_id: process.env.CLIENT_ID,
+  project_id: process.env.PROJECT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
+  auth_uri: "https://accounts.google.com/0/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris: ["https://webcodejunkie.github.io/meet/"],
+  javascript_origins: ["https://webcodejunkie.github.io", "http://localhost:3030", "http://localhost:8080"],
+};
+
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
+
+// exported function to get the authorized URL
+
+module.exports.getAuthURL = async () => {
+
+  const authUrl = oAuth2Client.generateAuthUrl({
+
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      authUrl: authUrl,
+    }),
+  };
+};
+
+
+// exported module function that'll give an token
+
+module.exports.getAccessToken = async (event) => {
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    /**
+     * Exhange authorization code for access token with a "callback" after the exhange,
+     * The callback in this case is an arrow function with the results as paramters: "err" and "token.""
+     */
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(token);
+    });
+  })
+    .then((token) => {
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(token),
+      };
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
+};
+
+// exported function that gets events
+module.exports.getCalendarEvents = async (event) => {
+
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((reject, resolve) => {
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      }
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    })
+}
---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />)
+  });
+
+  test('test if the select element is rendering', () => {
+    expect(NumberOfEventsWrapper.find('.event-options')).toHaveLength(1);
+  });
+
+  test('test by default if number of events is set to default', () => {
+    expect(NumberOfEventsWrapper.state('selectedOption')).toBe(null);
+  });
+
+  test('test the change when a user selects a new option', () => {
+    NumberOfEventsWrapper.setState({
+      selectedOption: null
+    });
+    NumberOfEventsWrapper.find('.event-options').simulate('click');
+    NumberOfEventsWrapper.setState({
+      selectedOption: { numOfEvents: '16' }
+    });
+    expect(NumberOfEventsWrapper.state('selectedOption')).toEqual({ numOfEvents: '16' });
+  });
+
+});
