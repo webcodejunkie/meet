fb78b8e9d7ace2de789ab8a4f0e1aad5ef154a3f
webcodejunkie <webcodejunkie@gmail.com>
2021-12-29 19:16:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import * as serviceWorker from './service-Worker';
+import * as serviceWorker from './service-worker';
 import * as atatus from 'atatus-spa';
 import reportWebVitals from './reportWebVitals';
 
@@ -19,6 +19,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
